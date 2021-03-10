4708ec7d4f1dc466507091768dc8dc3fe0b3956a
Jakub Zarębski <jakubzarebski0@gmail.com>
2021-03-10 13:11:36 +0100

    Added switching option between Farenheit an Celcius

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 window.addEventListener("load", function(){
+    
     let locationTimzone = document.querySelector(".location-timezone");
     let temperatureDegree = document.querySelector(".temperature-degree");
     let temperatureDescription = document.querySelector(".tempereture-description");
     let iconSrc = document.querySelector(".icon");
-    let feelsLike = document.querySelector(".feels-like")
+    let feelsLike = document.querySelector(".feels-like");
+    let degreeSection = document.querySelector(".temperature");
+    let degreeType = document.querySelector(".temp-type");
+    let feelType = document.querySelector(".feel-type");
     // Get current location
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -22,16 +26,32 @@ window.addEventListener("load", function(){
                  const {temperature, weather_descriptions, weather_icons, feelslike} = data.current;
                  const {name} = data.location;
                  // Set DOM elements from api
-                 temperatureDegree.textContent = `Current ${temperature} C`;
+                 temperatureDegree.textContent = `Current ${temperature}`;
                  locationTimzone.textContent = name;
                  temperatureDescription.textContent = weather_descriptions.join(", ");
                  iconSrc.src = weather_icons[0];
                  feelsLike.textContent = `Feels like ${feelslike}`;
+                 //switch degree type  C / F
+                 degreeSection.addEventListener("click", function(){
+                     if (degreeType.textContent === "C"){
+                        let tempF = temperature * 1.8000 + 32;
+                        let feelF = feelslike * 1.800 + 32;
+                        degreeType.textContent = "F";
+                        feelType.textContent = "F";
+                        temperatureDegree.textContent = `Current ${tempF}`;
+                        feelsLike.textContent = `Feels like ${feelF}`;
+
+                     } else if (degreeType.textContent === "F"){
+                         degreeType.textContent ="C";
+                         feelType.textContent = "F";
+                         temperatureDegree.textContent = `Current ${temperature}`;
+                         feelsLike.textContent = `Feels like ${feelslike}`;
+                     }
+                    
+                    });
             });
         });
     }
- // degree type switch C / F
- let degreeSection = document.querySelector(".degree-section");
  
 
 });
